db23fc89910630787780ba82abcdd315c1038e0a
sandhya-456T <sandhyathapaliya995@gmail.com>
2025-09-15 12:16:00 +0930

    displaying the details

--- lightglobe.py
+++ lightglobe.py
@@ -9,3 +9,11 @@ class LightGlobe(Light):
 
     def duplicate(self):
         return LightGlobe(self.color, self.voltage, self.current_ma, self.price)
+    
+    def display_the_details(self) -> str:
+        c = self.color.capitalize()
+        return "{:.1f}".format(self.voltage) + "V " + "{:.1f}".format(self.current_ma) + "mA " + c + " Light Globe $" + "{:.2f}".format(self.price)
+
+    @classmethod
+    def parse_csv(cls, csv_str: str):
+        pass
